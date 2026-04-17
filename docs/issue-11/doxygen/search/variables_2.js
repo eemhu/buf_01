79469d65db399a7488c217074f4b9e46db658694
eemhu <125959687+eemhu@users.noreply.github.com>
2026-04-17 11:36:56 +0000

    Deploying to gh-pages from @ eemhu/buf_01@97a6e4dbb1f6a793838d1bb796877648d8b14a96 🚀

--- docs/issue-11/doxygen/search/variables_2.js
+++ docs/issue-11/doxygen/search/variables_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['close_0',['close',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1pool_1_1DebugMemorySegmentLeasePool.html#ac7bfd14292968f89088375a96e4ac5df',1,'com::teragrep::buf_01::buffer::pool::DebugMemorySegmentLeasePool']]],
-  ['currentoffset_1',['currentOffset',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1TrackedMemorySegmentLease.html#a3b56b972519f70a504122ecd0970c059',1,'com::teragrep::buf_01::buffer::lease::TrackedMemorySegmentLease']]]
+  ['currentoffset_1',['currentOffset',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1TrackedMemorySegmentLease.html#a1a48b0e7917dec3ec4f1c30a8351b0a4',1,'com::teragrep::buf_01::buffer::lease::TrackedMemorySegmentLease']]]
 ];
