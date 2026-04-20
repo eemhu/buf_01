3e06cfc742d0f539b4445727f28f09ea4c5821ad
eemhu <125959687+eemhu@users.noreply.github.com>
2026-04-20 12:49:14 +0000

    Deploying to gh-pages from @ eemhu/buf_01@b7a89063272e26ae38cbd35b0a432efd0756f819 🚀

--- docs/issue-11/doxygen/search/variables_6.js
+++ docs/issue-11/doxygen/search/variables_6.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['offsetupdater_0',['offsetUpdater',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1TrackedMemorySegmentLease.html#abad4d1a8247cc6173863cabc42b72da8',1,'com::teragrep::buf_01::buffer::lease::TrackedMemorySegmentLease']]],
-  ['origin_1',['origin',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1TrackedMemorySegmentLease.html#a4e71fd31d5258e335ae2044aef57e18d',1,'com.teragrep.buf_01.buffer.lease.TrackedMemorySegmentLease.origin'],['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1pool_1_1OpeningPool.html#ae994e09af070baafb376041bc59c56be',1,'com.teragrep.buf_01.buffer.pool.OpeningPool.origin']]]
+  ['origin_0',['origin',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1TrackedMemorySegmentLease.html#a4e71fd31d5258e335ae2044aef57e18d',1,'com.teragrep.buf_01.buffer.lease.TrackedMemorySegmentLease.origin'],['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1pool_1_1OpeningPool.html#ae994e09af070baafb376041bc59c56be',1,'com.teragrep.buf_01.buffer.pool.OpeningPool.origin']]]
 ];
