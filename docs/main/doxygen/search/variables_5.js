82155dae0b147e201066a5c19f7db6ac2f854e8e
eemhu <125959687+eemhu@users.noreply.github.com>
2026-06-29 11:05:34 +0000

    Deploying to gh-pages from @ eemhu/buf_01@dd51546522fceed687466a8eec45a94846fd62ac 🚀

--- docs/main/doxygen/search/variables_5.js
+++ docs/main/doxygen/search/variables_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['memorysegment_0',['memorySegment',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1container_1_1MemorySegmentContainerImpl.html#a13016abb00624ec9d83468b3e5c60d1d',1,'com::teragrep::buf_01::buffer::container::MemorySegmentContainerImpl']]],
-  ['memorysegmentcontainer_1',['memorysegmentcontainer',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1MemorySegmentLease.html#af81ff99e19c508736c2841dc87cf658d',1,'com.teragrep.buf_01.buffer.lease.MemorySegmentLease.memorySegmentContainer'],['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1MemorySegmentSubLease.html#ad43b32e079aa884311c7de768241bed1',1,'com.teragrep.buf_01.buffer.lease.MemorySegmentSubLease.memorySegmentContainer']]]
+  ['mark_0',['mark',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1TrackedMemorySegmentLease.html#aa83b37df554030d483e44d5fdb1f1072',1,'com::teragrep::buf_01::buffer::lease::TrackedMemorySegmentLease']]],
+  ['memorysegment_1',['memorySegment',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1container_1_1MemorySegmentContainerImpl.html#a13016abb00624ec9d83468b3e5c60d1d',1,'com::teragrep::buf_01::buffer::container::MemorySegmentContainerImpl']]],
+  ['memorysegmentcontainer_2',['memorysegmentcontainer',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1MemorySegmentLease.html#af81ff99e19c508736c2841dc87cf658d',1,'com.teragrep.buf_01.buffer.lease.MemorySegmentLease.memorySegmentContainer'],['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1MemorySegmentSubLease.html#ad43b32e079aa884311c7de768241bed1',1,'com.teragrep.buf_01.buffer.lease.MemorySegmentSubLease.memorySegmentContainer']]]
 ];
